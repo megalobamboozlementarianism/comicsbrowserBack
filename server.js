bfd443d1958aa42d60eb155aee3ba9f90696010f
megalobamboozlementarianism <abe.coley@gmail.com>
2019-04-08 11:43:12 -0600

    set up separate back end because reasons

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+
+const { PORT } = process.env;
+
+function listenCB () {
+    console.log('now we\'re running server on port: ' + PORT);
+}
+
+// Now let's add the static files with express middleware
+
+app.use(cors());
+app.get('/', (req, res)=>{ 
+  axios.get(req._parsedUrl.query)
+  .then(function(response) {
+    res.status(200);
+    res.json(response.data)
+  })
+  .catch(function (error) {
+  }); 
+})
+
+app.listen(PORT, listenCB);
